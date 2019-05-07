5d3cc860bfa771dd116f9f7d07fff959b0b272db
Bradyn Glines <glinesbdev@gmail.com>
2019-05-06 20:40:01 -0500

    Config port for Heroku

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+const port = process.env.PORT || 8080;
+
 app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-
-app.listen(3000);
-
-console.log('Server running at http://localhost:3000');
+app.listen(port, () => {
+    console.log('Server running at http://localhost:' + port);
+});
